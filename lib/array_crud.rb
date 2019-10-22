61e6469a77cafe5ad5957feb9ec9b3d54c6db7c4
Alice Won <alicewon4@gmail.com>
2019-10-22 21:45:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  new_array= [tacos, pizza, cookies, salad]
+  new_array= ["tacos", "pizza", "cookies", "salad"]
 end
 
 def add_element_to_end_of_array(array, element)
