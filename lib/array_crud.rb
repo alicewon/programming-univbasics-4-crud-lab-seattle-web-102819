0fe2b88f380bc7aa880a30d1d97d764a413ff1b4
Alice Won <alicewon4@gmail.com>
2019-10-22 21:48:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  new_array= ["tacos", "pizza", "cookies", "salad"]
   new_array << "breadsticks"
+  p new_array
 end
 
 def add_element_to_start_of_array(array, element)
